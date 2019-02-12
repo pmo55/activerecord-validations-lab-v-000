14c0734f0e1d5f560c4efd7bae1946bacd3126bf
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-02-12 15:57:20 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {:minimum => 250}
+  validates :summary, length: {:maximum => 250}
+  validates :category, inclusion: {in: %w(Fiction Non Fiction)}
+  validate :clickbait?
+  
+  CLICKBAIT = [/won't believe/i, /secret/i, /guess/i, /top [0-9]*/i]
+  
+  def clickbait?
+    if CLICKBAIT.none? {|phrase|phrase.match title}
+      errors.add(:title, "Do better")
+    end
+  end
 end
