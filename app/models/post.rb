c81b903c99e139f2afcc12ae97373bc60858c221
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-02-12 15:53:09 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,5 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: {in: %w(Fiction Non Fiction)}
   validate :clickbait?
   
-  CLICKBAIT = [/won't believe/i,/secret/i,/guess/i, /top/i]
+  CLICKBAIT = [/won't believe/i,/secret/i,/guess/i, /top [0-9]*/i]
 end
